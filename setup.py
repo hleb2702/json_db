fdef507762606124097e61d2cb9383ed7613b8b0
hleb2702 <glebstetko2@gmail.com>
2023-10-30 15:17:10 +0300

    0.0.1

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-from config import *
+from config import email
 
 with open('README.md', encoding='utf-8') as f:
     long_dcs2 = f.read()
@@ -17,7 +17,7 @@ setup(
     long_description=long_dcs2,
     url='https://github.com/hleb2702/json_db.git',
 
-    download_url=f'https://github.com/hleb2702/jdon_db_python/archive/refs/tags/jdon_db.zip',
+    download_url='https://github.com/hleb2702/json_db/archive/refs/heads/main.zip',
 
     license='Apache License, Version 2.0, see LICENSE file',
     packages=find_packages(),
