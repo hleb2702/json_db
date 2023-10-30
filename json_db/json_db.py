23d1a417e833ffd6a25765507e170d8ad59b16b8
hleb2702 <glebstetko2@gmail.com>
2023-10-30 12:00:43 +0300

    first commit

---
+++ json_db/json_db.py
@@ -0,0 +1,13 @@
+'''
+:authors: Hleb0227
+:license: Apache License, Version 2.0, see LICENSE file
+
+:copyright: (c) 2023 Hleb0227
+'''
+
+from json import dumps, loads
+from json_db_exceptions import JsonDBError
+
+class json_db:
+    pass
+
